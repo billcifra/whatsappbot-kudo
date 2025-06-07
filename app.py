d0e50171a10e990509912ba290cc564f16b41139
billcifra <manu_rioja@hotmail.com>
2025-06-06 20:47:20 -0400

    Primer commit limpio sin secretos

--- app.py
+++ app.py
@@ -273,4 +273,4 @@ def webhook():
 # Inicio del servidor Flask
 # ---------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
